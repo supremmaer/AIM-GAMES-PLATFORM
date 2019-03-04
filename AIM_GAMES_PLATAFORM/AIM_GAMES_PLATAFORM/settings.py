bd072ca7b8b83e7ecd502c26b1cd8332c6d9c1d2
AlejandroMonteseirin <alexmonteseirin@gmail.com>
2019-03-04 16:52:58 +0100

    fuentes fix

--- AIM_GAMES_PLATAFORM/AIM_GAMES_PLATAFORM/settings.py
+++ AIM_GAMES_PLATAFORM/AIM_GAMES_PLATAFORM/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
