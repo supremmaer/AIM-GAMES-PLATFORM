05f91ab9c2204d1bbee5c4327eb876a454c39730
Miguel Santos Batista <rawnnnnn@gmail.com>
2019-03-21 01:59:30 +0100

    Update ModelForm for signup

--- AIM_GAMES_PLATFORM/AIM_GAMES/forms.py
+++ AIM_GAMES_PLATFORM/AIM_GAMES/forms.py
@@ -1,9 +1,11 @@
-from django.forms import ModelForm, forms
+from django.forms import ModelForm, forms, CharField, EmailField
+from django.contrib.auth.forms import UserCreationForm
 from AIM_GAMES.models import Freelancer, Business, Profile
+from django.contrib.auth.models import User
 
 
 class FreelancerForm(ModelForm):
-    # TODO Lógica
+    # TODO Testear salvado
     class Meta:
         model = Freelancer
         exclude = ()
@@ -13,7 +15,7 @@ class FreelancerForm(ModelForm):
         self.fields['profile'].required = False
         data = kwargs.get('data')
         # 'prefix' parameter required if in a modelFormset
-        self.profile_form = ProfileForm(instance=self.instance and self.instance.profile, prefix=self.prefix, data=data)
+        self.profile_form = ProfileForm(instance=self.instance, prefix=self.prefix, data=data)
 
     def clean(self):
         if not self.profile_form.is_valid():
@@ -34,7 +36,31 @@ class BusinessForm(forms.Form):
 
 
 class ProfileForm(ModelForm):
-    # TODO Lógica
+    # TODO Testear salvado
     class Meta:
         model = Profile
         exclude = ()
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.fields['user'].required = True
+        data = kwargs.get('data')
+        # 'prefix' parameter required if in a modelFormset
+        self.user_form = UserForm(instance=self.instance, prefix=self.prefix, data=data)
+
+    def clean(self):
+        if not self.user_form.is_valid():
+            raise forms.ValidationError("User not valid")
+
+    def save(self, commit=True):
+        obj = super(ProfileForm, self).save(commit=commit)
+        obj.user = self.user_form.save()
+        obj.save()
+
+
+class UserForm(UserCreationForm):
+    # TODO Testear salvado
+
+    class Meta:
+        model = User
+        fields = ['username']
