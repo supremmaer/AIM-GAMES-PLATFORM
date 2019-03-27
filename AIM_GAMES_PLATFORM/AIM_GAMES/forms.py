77cf0c31dd9336e45896b839a9cd22b54b65653b
Miguel Santos Batista <miguelsanbat@gmail.com>
2019-03-27 21:43:38 +0100

    Added check age

--- AIM_GAMES_PLATFORM/AIM_GAMES/forms.py
+++ AIM_GAMES_PLATFORM/AIM_GAMES/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User, Group
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
+from datetime import datetime,timedelta
 
 
 class BusinessForm(ModelForm):
@@ -74,7 +75,7 @@ class ProfileForm(ModelForm):
     postalCode = CharField(widget=NumberInput(), label=_("Postal Code"))
     idCardNumber = CharField(widget=TextInput(), label=_("IDCard Number"))
     dateOfBirth = DateField(widget=DateInput(), label=_("Date of birth"))
-    phoneNumber = CharField(widget=NumberInput(), label=_("Postal Code"))
+    phoneNumber = CharField(widget=NumberInput(), label=_("Phone Number"))
     photo = URLField(widget=URLInput(), label=_("Photo URL:"))
 
     class Meta:
@@ -89,6 +90,23 @@ class ProfileForm(ModelForm):
         # 'prefix' parameter required if in a modelFormset
         self.instance.user = User()
         self.user_form = UserForm(instance=self.instance and self.instance.user, prefix=self.prefix, data=data)
+
+    def clean_phoneNumber(self):
+        print('clean: ProfileForm: PhoneNumber')
+        data = self.cleaned_data['phoneNumber']
+        if not data.isdigit():
+            raise ValidationError(_("Phone Number must be a number"))
+        return data
+
+    def clean_dateOfBirth(self):
+        print('clean: ProfileForm: dateOfBityh')
+        data = self.cleaned_data['dateOfBirth']
+        from_date = datetime.now() - timedelta(days=18*365)
+        print(str(from_date))
+        print(str(data))
+        if data > datetime.date(from_date):
+            raise ValidationError(_("You must be over 18 to sign up"))
+        return data
 
     def clean(self):
         print('clean: ProfileForm')
