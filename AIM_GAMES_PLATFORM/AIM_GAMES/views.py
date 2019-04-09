f173c4983b21cdcdb51832fda6827c959f1f1779
AlejandroMonteseirin <alexmonteseirin@gmail.com>
2019-04-09 21:28:34 +0200

    desloguea y envia a el login al borrar

--- AIM_GAMES_PLATFORM/AIM_GAMES/views.py
+++ AIM_GAMES_PLATFORM/AIM_GAMES/views.py
@@ -830,7 +830,8 @@ def downloadData(request):
 def deleteUser(request):
     user=request.user
     user.delete()
-    return render(request, 'index.html')
+    auth.logout(request)
+    return render(request, 'accounts/login.html')
 
 
 def message_list(request):
