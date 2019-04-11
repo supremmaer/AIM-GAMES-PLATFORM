c8eb0d78ef54feb23dd2d53f389bdad015c6d053
Supremmaer <fjdelafuenteb@gmail.com>
2019-04-11 17:37:23 +0200

    Secure paypal deploy test #3

--- AIM_GAMES_PLATFORM/AIM_GAMES/views.py
+++ AIM_GAMES_PLATFORM/AIM_GAMES/views.py
@@ -38,12 +38,13 @@ def pagarPaypal(request):
     print(businessId)
     #local notify url
     #'notify_url': 'https://dbda170f.ngrok.io/paypal_ipn/'+str(businessId),
+    
     paypal_dict = {
         'business': settings.PAYPAL_RECEIVER_EMAIL,
         'amount': '71',
         'item_name': 'Subscripcion AIM-GAMES',
         'currency_code': 'EUR',
-        'notify_url': 'http://{}{}{}'.format(host, 'paypal_ipn/',str(businessId)),
+        'notify_url': 'https://aim-games-2.herokuapp.com/paypal_ipn/'+str(businessId),
         'return_url': 'http://{}{}'.format(host, reverse('payment_done')),
         'cancel_return': 'http://{}{}'.format(host, reverse('payment_canceled')),
         }
